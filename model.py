d1e02b57d28ce8b3aa97b27bd16473eb8ed92d44
Emmelie Gren <emmelierod@gmail.com>
2023-01-03 17:57:02 +0100

    Changed names on Yarn

--- model.py
+++ model.py
@@ -12,8 +12,8 @@ class Produkter(db.Model):
 
 
 def seedData(db):
-    namn = ["Ull Garn","Syntet Garn", "Bomulls Garn", "Lin Garn", "Bland Garn"]
-    color = ["Beige", "Grå", "Svart", "Brun", "Amber"]
+    namn = ["Ullgarn","Syntetgarn", "Bomullsgarn", "Lingarn", "Blandgarn", "Mohairgarn", "Cashmeregarn"]
+    color = ["Beige", "Grå", "Svart", "Brun", "Amber", "Ljusgrå", "Congac", "Smaragdgrön", "Senapsgul", "Rost", "Vit", "Natur" ]
     antal =  Produkter.query.count()
     while antal < 500:
         produkt = Produkter()
@@ -24,34 +24,3 @@ def seedData(db):
         db.session.commit()
 
 
-
-
-
-
-# import random
-# from flask_sqlalchemy import SQLAlchemy
-
-# db = SQLAlchemy()
-
-# class Customer(db.Model):
-#     __tablename__= "Customer"
-#     Id = db.Column(db.Integer, primary_key=True)
-#     Name = db.Column(db.String(50), unique=False, nullable=False)
-#     City = db.Column(db.String(40), unique=False, nullable=False)
-#     TelephoneCountryCode = db.Column(db.Integer, unique=False, nullable=False)
-#     Telephone = db.Column(db.String(20), unique=False, nullable=False)
-
-
-# def seedData(db):
-#     cites = ["Stockholm","Västerås", "Södertälje"]
-#     countrycodes = [46,47,44]
-#     antal =  Customer.query.count()
-#     while antal < 500:
-#         customer = Customer()
-#         customer.Name = "Customer" + str(antal)
-#         customer.TelephoneCountryCode = random.choice(countrycodes)
-#         customer.Telephone = random.randint(10000000,999999999)
-#         customer.City = random.choice(cites)
-#         db.session.add(customer)
-#         db.session.commit()
-#         antal = antal + 1
